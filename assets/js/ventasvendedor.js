0b09b20e3f40aad84d701fa7c75838ca2145ae82
adimerpaul <adimer@gmail.com>
2019-04-09 10:26:25 -0400

    ventas vendedor

---
+++ assets/js/ventasvendedor.js
@@ -0,0 +1,18 @@
+
+var start = moment().subtract(29, 'days');
+var end = moment();
+    $('#fecha').daterangepicker({
+    startDate: start,
+    endDate: end,
+    ranges: {
+        'Hoy': [moment(), moment()],
+        'Ayer': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    }
+}, function(start, end, label) {
+    console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+    $('#fecha span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+}).click();;
