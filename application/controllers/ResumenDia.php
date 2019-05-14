947f4ba3d11930f9c10230a946de99213d1181dd
adimerpaul <adimer@gmail.com>
2019-05-14 10:30:15 -0400

    terminado caja

--- application/controllers/ResumenDia.php
+++ application/controllers/ResumenDia.php
@@ -1,6 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+require_once('tcpdf.php');
+include "qrlib.php";
+include "NumerosEnLetras.php";
+require 'autoload.php';
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\EscposImage;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
@@ -28,13 +31,14 @@ class ResumenDia extends CI_Controller {
         else redirect('');
     }
     public function imprimir(){
+        /*
 
         $nombre_impresora = "POS";
 
         $connector = new WindowsPrintConnector($nombre_impresora);
         $printer = new Printer($connector);
 
-        /* Initialize */
+
         $printer -> initialize();
         $ca = "MULTI CINES PLAZA SRL.
 Av. Tacna y Jaen - Oruro -Bolvia
@@ -60,9 +64,58 @@ ORURO - BOLIVIA
         $ca="El total es: $total";
         $printer->text($ca);
         $printer -> setJustification(Printer::JUSTIFY_CENTER);
-        $ca="          ENTREGE CONFORME              RECIBI CONFORME";
+        $ca="          ENTREGE CONFORME              RECIBI CONFORME";$printer -> cut();
         $printer->text($ca);
+        $printer -> cut();
+        $printer -> close();
+        */
+        $nombre_impresora = "POS";
 
+
+        $connector = new WindowsPrintConnector($nombre_impresora);
+        $printer = new Printer($connector);
+
+        /* Initialize */
+        $printer -> initialize();
+
+        /* Text */
+//$printer -> text("Hello world\n");
+//$printer -> cut();
+        // set some text to print
+
+        $ca = "MULTI CINES PLAZA SRL.
+Av. Tacna y Jaen - Oruro -Bolvia
+ Tel: 591-25281290
+ORURO - BOLIVIA
+-------------------------------
+";
+        $printer -> setJustification(Printer::JUSTIFY_CENTER);
+        $printer->text($ca);
+        $printer -> setJustification(Printer::JUSTIFY_CENTER);
+        $printer->text($ca);
+        $printer -> setJustification(Printer::JUSTIFY_LEFT);
+        $html = "Fecha: ".date('Y-m-d h:m:s');
+        $printer -> text($html."\n");
+        $html = "Usuario: ".$_SESSION['nombre']."\n";
+        $printer -> text($html."\n");
+        $printer->text("NUMERRO    HORA      CLIENTE    TOTAL.\n");
+        $printer->text("-----------------------------------"."\n");
+        $total=0;
+        $query=$this->db->query("SELECT * FROM venta v INNER JOIN cliente c ON v.idcliente=c.idcliente
+                WHERE date(fechaVenta)=date('".date('Y-m-d')."')");
+        foreach ($query->result() as $row){
+            $printer->text( "    $row->idVenta  ".substr($row->fechaVenta,10,10)."         $row->apellidoCl     $row->total   \n");
+            $total=$total+$row->total;
+        }
+        $printer -> setJustification(Printer::JUSTIFY_RIGHT);
+        $ca = "\nTOTAL: $total\n";
+        $printer->text($ca);
+        $printer -> setJustification(Printer::JUSTIFY_CENTER);
+        $ca = "\n\n\nENTREGE CONFORME              RECIBI CONFORME\n";
+        $printer->text($ca);
+        $printer -> cut();
+        $printer -> close();
+        header("Location: ".base_url()."ResumenDia");
     }
 
 }
