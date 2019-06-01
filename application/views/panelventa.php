db6f4341363c4bdcbbe377d290a04db84cdda590
Alejandro <lopez.gutierrez.a07@gmail.com>
2019-06-01 10:30:43 -0400

    apellido

--- application/views/panelventa.php
+++ application/views/panelventa.php
@@ -312,14 +312,14 @@
   </div>
 
   <div class="form-row">
+  <div class="form-group col-md-6">
+      <label for="apellido">Apellido:</label>   
+      <input type="text" class="form-control" id="apellido" name="apellido" Required style="text-transform: uppercase;">  
+    </div>
     <div class="form-group col-md-6">
       <label for="nombre">Nombre:</label>   
       <input type="text" class="form-control" id="nombre" name="nombre"  style="text-transform: uppercase;">  
       <input type="hidden" id="idcliente" name="idcliente">  
-    </div>
-    <div class="form-group col-md-6">
-      <label for="apellido">Apellido:</label>   
-      <input type="text" class="form-control" id="apellido" name="apellido" Required style="text-transform: uppercase;">  
     </div>
   </div>
 
